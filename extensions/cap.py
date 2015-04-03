c76f591dbe4a1518636a909ff86f6ac86cb89c73
Elizabeth Myers <elizabeth@interlinked.me>
2015-04-03 11:28:15 -0500

    cap: support IRC 3.2 extensions

--- extensions/cap.py
+++ extensions/cap.py
@@ -13,14 +13,21 @@ from base import BaseExtension, PRIORITY_FIRST, EVENT_CONNECTED, EVENT_CANCEL
 from numerics import Numerics
 
 
+EVENT_CAP_ACK_PRE = 3  # CAP ACK (pre) event
+EVENT_CAP_ACK_POST = 4  # CAP ACK (post) event
+
+
 logger = getLogger(__name__)
 
 
 class CapNegotiate(BaseExtension):
 
-    """ Basic CAP negotiation """
+    """ Basic CAP negotiation (version 302) """
 
     priority = PRIORITY_FIRST
+    cap_version = "302"
+
+    requires = ["BasicRFC"]
 
     def __init__(self, base, **kwargs):
 
@@ -42,16 +49,14 @@ class CapNegotiate(BaseExtension):
             "end" : self.cap_end,
         }
 
-        self.requires = ["BasicRFC"]
-
         # What we support - other extensions can add doodads to this
-        self.cap_supported = set()
+        self.cap_supported = dict()
 
         # What they support
-        self.cap_remote = set()
+        self.cap_remote = dict()
 
         # What we are actually using
-        self.cap_local = set()
+        self.cap_local = dict()
 
         # Negotiation phase
         self.negotiating = True
@@ -59,13 +64,38 @@ class CapNegotiate(BaseExtension):
         # Timer for CAP disarm
         self.cap_timer = None
 
+    def extract_caps(self, line):
+        """ Extract caps from a line """
+
+        caps = (self.parse_cap(cap) for cap in line.params[-1].split())
+        return {cap : param for cap, param in caps}
+
+    def parse_cap(self, string):
+        """ Parse a capability string """
+
+        cap, sep, param = string.partition('=')
+        if not sep:
+            return (cap, [])
+
+        return (cap, param.split(','))
+
+    def create_cap_str(self, d, cap):
+        """ Create a capability string """
+
+        if cap not in d:
+            return None
+        elif not d[cap]:
+            return cap
+
+        return "{}={}".format(cap, ','.join(d[cap]))
+
     def send_cap(self):
         """ Request capabilities from the server """
 
         logger.debug("Requesting CAP list")
 
-        self.send("CAP", ["LS"])
-        
+        self.send("CAP", ["LS", self.cap_version])
+
         self.cap_timer = self.schedule(15, self.cap_end)
 
         # Ensure no others get fired
@@ -91,16 +121,16 @@ class CapNegotiate(BaseExtension):
     def cap_get_remote(self, line):
         """ A list of the CAPs the server supports (CAP LS) """
 
-        cap_remote = set(line.params[-1].lower().split())
-        self.cap_remote = cap_remote
+        self.cap_remote = cap_remote = self.extract_caps(line)
 
         if self.negotiating:
             # Negotiate caps
             cap_supported = self.cap_supported
-            supported = cap_supported.intersection(cap_remote)
+            supported = [self.create_cap_str(c, v) for c, v in
+                         cap_remote.items() if c in cap_supported]
 
             if supported:
-                caps = ' '.join(sorted(supported)).lower()
+                caps = ' '.join(sorted(supported))
                 logger.debug("Requesting caps: %s", caps)
                 self.send("CAP", ["REQ", caps])
             else:
@@ -111,40 +141,43 @@ class CapNegotiate(BaseExtension):
     def cap_get_local(self, line):
         """ caps presently in use """
 
-        caps = line.params[-1].lower()
+        self.caps_local = caps = extract_caps(line)
         logger.debug("CAPs active: %s", caps)
-        self.cap_local = set(caps.split())
 
     def cap_ack(self, line):
         """ Acknowledge a CAP ACK response """
 
-        for cap in line.params[-1].lower().split():
+        # Allow extensions to register their own stuff
+        if self.dispatch_event(self.hooks, EVENT_CAP_ACK_PRE) is not None:
+            return
+
+        for cap, params in self.extract_caps(line):
             if cap.startswith('-'):
                 cap = cap[1:]
                 logger.debug("CAP removed: %s", cap)
-                self.cap_local.discard(cap)
+                self.cap_local.pop(cap, None)
                 continue
             elif cap.startswith(('=', '~')):
-                # Just strip out these for now
-                # TODO - handle sticky/required bits correctly
+                # Compatibility stuff
                 cap = cap[1:]
 
             assert cap in self.cap_supported
             logger.debug("Acknowledged CAP: %s", cap)
-            self.cap_local.add(cap)
+            self.cap_local[cap] = params
 
-        if self.negotiating:
-            # Negotiation ends
-            self.cap_end()
+        if self.dispatch_event(self.hooks, EVENT_CAP_ACK_POST) is None:
+            if self.negotiating:
+                # Negotiation ends
+                self.cap_end()
 
     def cap_nak(self, line):
         """ CAPs rejected """
 
         logger.warn("Rejected CAPs: %s", line.params[-1].lower())
-    
+
     def cap_end(self, line=None):
         """ End the CAP process """
-        
+
         logger.debug("Ending CAP negotiation")
 
         if self.cap_timer:

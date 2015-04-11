ebf5f2d5306a0cf698e9f451114c215546eec9f4
Elizabeth Myers <elizabeth@interlinked.me>
2015-04-11 11:21:27 -0500

    casemapping: add docstrings

--- PyIRC/casemapping.py
+++ PyIRC/casemapping.py
@@ -3,7 +3,17 @@
 # for licensing information.
 
 
-""" IRC casefoldping """
+r""" IRC-style casemapping and casefolding
+
+IRC uses two forms of casemapping: RFC1459 and ASCII. Unicode is not handled
+or casemapped by any known servers. 
+
+ASCII is simply casemapped as "abc..." <-> "ABC...".
+
+RFC1459 is the same as ASCII in terms of casefolding semantics, except that
+the characters {}|- are the lowercase equivalents of []\^. This is due to an
+historical wart in the protocol.
+"""
 
 
 from string import ascii_lowercase, ascii_uppercase
@@ -22,13 +32,27 @@ ascii_toupper = str.maketrans(ascii_uppercase, ascii_lowercase)
 
 
 class IRCString(str):
-    """ An IRC string """
+    """ An IRC string.
+    
+    Same as a normal string, with IRC style casemapping.
+    """
 
     UNICODE = 0
     ASCII = 1
     RFC1459 = 2
 
     def __new__(cls, s, case):
+        """Create a new IRC String.
+
+        Arguments:
+
+        s
+            String to use
+
+        case
+            Casemapping, can be UNICODE, ASCII, or RFC1459. This controls the
+            behaviour of the irc_* functions.
+        """
         self = super(IRCString, cls).__new__(cls, s)
 
         if case == IRCString.ASCII:
@@ -48,19 +72,31 @@ class IRCString(str):
         return self
 
     def ascii_lower(self):
+        """Return a copy of the string S converted to lowercase, using ASCII
+        semantics."""
         return str.translate(self, ascii_tolower)
 
     def ascii_upper(self):
+        """Return a copy of the string S converted to uppercase, using ASCII
+        semantics."""
         return str.translate(self, ascii_toupper)
 
     def ascii_casefold(self):
+        """Return a version of S suitable for caseless comparisons, using
+        ASCII semantics."""
         return str.translate(self, ascii_tolower)
 
     def rfc1459_lower(self):
+        """Return a copy of the string S converted to lowercase, using RFC1459
+        semantics."""
         return str.translate(self, rfc1459_tolower)
 
     def rfc1459_upper(self):
+        """Return a copy of the string S converted to uppercase, using RFC1459
+        semantics."""
         return str.translate(self, rfc1459_toupper)
 
     def rfc1459_casefold(self):
+        """Return a version of S suitable for caseless comparisons, using
+        RFC1459 semantics."""
         return str.translate(self, rfc1459_tolower)

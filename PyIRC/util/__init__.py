67ed40c66c0b4ea32f02f19fcdfddca627ca534e
Elizabeth Myers <elizabeth@interlinked.me>
2015-04-14 09:55:26 -0500

    Add proper __init__.py files with __all__

---
+++ PyIRC/util/__init__.py
@@ -0,0 +1,7 @@
+# Copyright © 2013-2015 Elizabeth Myers.  All rights reserved.
+# This file is part of the PyIRC 3 project. See LICENSE in the root directory
+# for licensing information.
+
+
+__all__ = ["classutil", "enum", "version"]
+

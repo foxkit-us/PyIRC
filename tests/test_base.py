bd43dab6a7d04ea66845ac0c7cc079eefe693e92
Elizabeth Myers <elizabeth@interlinked.me>
2015-04-15 08:29:08 -0500

    Add a unit test for doctested modules

---
+++ tests/test_base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright © 2015 Andrew Wilcox and Elizabeth Myers.
+# All rights reserved.
+# This file is part of the PyIRC 3 project. See LICENSE in the root directory
+# for licensing information.
+
+
+"""Exercise the base modules a bit"""
+
+
+import unittest
+import doctest
+
+from PyIRC import *
+
+
+# These have doctests
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(auxparse))
+    tests.addTests(doctest.DocTestSuite(line))
+    tests.addTests(doctest.DocTestSuite(casemapping))
+    return tests
+
+

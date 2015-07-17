5b7a05f1aa8de62f2e91dd9a53d1905798a7907d
Elizabeth Myers <elizabeth@interlinked.me>
2015-07-16 20:07:27 -0500

    get_all_subclasses: use a generator

--- PyIRC/util/classutil.py
+++ PyIRC/util/classutil.py
@@ -8,11 +8,6 @@
 
 
 def get_all_subclasses(cls):
-    all_subclasses = []
-
     for subclass in cls.__subclasses__():
-        all_subclasses.append(subclass)
-        all_subclasses.extend(get_all_subclasses(subclass))
-
-    return all_subclasses
-
+        yield subclass
+        yield from get_all_subclasses(subclass)

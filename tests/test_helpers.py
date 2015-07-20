c18e11b9ef91be856de5e6287b38b3dba97b8859
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2015-07-20 00:35:24 -0500

    Add useful doodads to test_helpers

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -8,7 +8,7 @@
 
 from PyIRC.extensions.basicrfc import BasicRFC
 from PyIRC.io.null import NullSocket
-from PyIRC.line import Line
+from PyIRC.line import Line, Hostmask
 
 
 def new_connection(username='TestUser', nick='Test', gecos='Test User',
@@ -18,13 +18,19 @@ def new_connection(username='TestUser', nick='Test', gecos='Test User',
     ns.connect()
     return ns
 
+
 def new_conn_with_handshake(username='TestUser', nick='Test', gecos='Test User',
                             extensions=[BasicRFC], **kwargs):
     ns = new_connection(username, nick, gecos, extensions, **kwargs)
     ns.draw_line()  # the USER
     ns.draw_line()  # and the NICK
     ns.inject_line(Line(command='001',
-                        params=(nick, 'Welcome to the test network %s' % nick)))
+                        params=(nick, 'Welcome to the Test network %s' % nick)))
     ns.inject_line(Line(command='002',
-                        params=(nick, 'Your host is a.non-existent.test.server')))
+                        params=(nick, 'Your host is nonexistent.test.server')))
     return ns
+
+
+def conn_mask(connection):
+    return Hostmask(nick=connection.nick, username=connection.username,
+                    host='test-connection.local')

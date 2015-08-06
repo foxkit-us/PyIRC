f0c01eaa097d0aa5aaed60a1d464b82e802fbea4
Elizabeth Myers <elizabeth@interlinked.me>
2015-08-05 22:21:22 -0500

    Add probing extension and machinery

--- PyIRC/extensions/ircd/__init__.py
+++ PyIRC/extensions/ircd/__init__.py
@@ -1,4 +1,18 @@
+# Copyright © 2013-2015 Elizabeth Myers.  All rights reserved.
+# This file is part of the PyIRC 3 project. See LICENSE in the root directory
+# for licensing information.
+
+
+"""IRC daemon specific extensions."""
+
+
 from collections import namedtuple
+
+from PyIRC.extensions import BaseExtension
+from PyIRC.numerics import Numerics
+from PyIRC.signal import event
+
+from PyIRC.util.classutil import get_all_subclasses
 
 
 __all__ = ["base", "hybridfamily"]
@@ -10,3 +24,27 @@ Extban = namedtuple("Extban", "negative ban target")
 
 BanEntry = namedtuple("BanEntry", "mask setter duration reason")
 """A result from a ban lookup"""
+
+
+# FIXME this sucks, importing them all here so the subclasses of BaseServer
+# are visible
+from PyIRC.extensions.ircd import base, hybridfamily
+
+
+class IRCDaemonExtension(BaseExtension):
+
+    """The extension for discovering IRC daemons and loading the correct
+    server-specific extension."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.extension_name = None
+        self.server = None
+
+    @event("commands", Numerics.RPL_VERSION, priority=1000)
+    def probe(self, _, _):
+        for subclass in get_all_subclasses(base.BaseServer):
+            if subclass.provides(self.base):
+                self.load_extension(subclass)
+                return
